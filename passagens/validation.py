ed4127d12fa5cf32da8aaf1ecd9cf830e000be44
Gabriel Pelicano <gapelicano97@gmail.com>
2022-02-27 14:11:24 -0300

    Atualizando...

--- passagens/validation.py
+++ passagens/validation.py
@@ -7,3 +7,13 @@ def campo_tem_algum_numero(valor_campo, nome_campo, lista_de_erros):
     """ Se tem algum numero no campo """
     if any(char.isdigit() for char in valor_campo):
         lista_de_erros[nome_campo] = ('Origem inválida: Não inclua números')
+
+def data_ida_maior_que_data_volta(data_ida, data_volta, lista_de_erros):
+    """ Verifica se data de ida é maior que data de volta """
+    if data_ida > data_volta:
+        lista_de_erros['data_volta'] = 'Data de volta não pode ser maior que data de ida'
+
+def data_ida_menor_data_atual(data_ida, data_pesquisa, lista_de_erros):
+    """ Verifica se data de ida é menor que data atual """
+    if data_ida < data_pesquisa:
+        lista_de_erros['data_ida'] = 'Data de ida não pode ser menor que data atual'
--- passagens/views.py
+++ passagens/views.py
@@ -1,21 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from passagens.forms import PassagemForms
+from passagens.forms import PassagemForms, PessoaForms
 
 def index(request):
     form = PassagemForms()
-    contexto = {'form':form}
+    pessoa_form = PessoaForms()
+    contexto = {'form':form, 'pessoa_form': pessoa_form}
     return render(request, 'index.html', contexto)
 
 def revisao_consulta(request):
     if request.method == 'POST':
         form = PassagemForms(request.POST)
+        pessoa_form = PessoaForms(request.POST)
         if form.is_valid():
-            contexto = {'form':form}
+            contexto = {'form':form, 'pessoa_form': pessoa_form}
             return render(request, 'minha_consulta.html', contexto)
         else:
             print('Form inválido')
-            contexto = {'form':form}
+            contexto = {'form':form, 'pessoa_form': pessoa_form}
             return render(request, 'index.html', contexto)
     else:
         return HttpResponse('Método inválido para essa operação: {}'
